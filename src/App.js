c20326a4ed38b6e814a1519af4167a15623a8f11
SAR0S <weorbit79@gmail.com>
2021-03-01 23:31:09 +0900

    Add list create and read

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import ScrollBox from './components/scrollBox/ScrollBox.js';
+import IterationSample from './components/iterationSample/IterationSample.js';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <ScrollBox ref={(ref) => this.scrollBox=ref}/>
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
+        <IterationSample />
       </div>
       );
   }
--- src/components/iterationSample/IterationSample.js
+++ src/components/iterationSample/IterationSample.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
 
 class IterationSample extends Component {
+    state = {
+        names: ['눈사람', '얼음','눈','바람'],
+        name: ''
+    };
+
+    handleChange = (e) => {
+        this.setState({
+            name: e.target.value
+        });
+    }
+
+    handleInsert = () => {
+        // names 배열에 값을 추가하고 name 값을 초기화
+        this.setState({
+            names: this.state.names.concat(this.state.name),
+            name: ''
+        });
+    }
+
     render() {
-        const names = ['눈사람', '얼음','눈','바람'];
-        const nameList = names.map(
-            (name) => (<li>{name}</li>)
+        const nameList = this.state.names.map(
+            (name, index) => (<li key={index}>{name}</li>)
         );
 
         return (
-            <ul>
+            <div>
+                <input 
+                    onChange={this.handleChange}
+                    value={this.state.name} />
+                <button onClick={this.handleInsert}>추가</button>
+                <ul>
                 {nameList}
-            </ul>
+                </ul>
+            </div>
         );
     }
 }
